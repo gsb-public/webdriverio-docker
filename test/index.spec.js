feb82d3b3ddb12a33a3ee1ba62a246fb86e4a55d
Greg Mercer (GBear) <gmercer@stanford.edu>
2019-03-02 14:05:35 -0800

    Fullpage screenshot working.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,13 @@
 describe('webdriver.io page', function() {
   it('should have the right title', function () {
 
-    browser.url('https://www.google.com');
+    browser.url('https://gsb.stanford.edu');
 
     const title = browser.getTitle();
     console.log('Title was: ' + title);
 
-    //browser.saveDocumentScreenshot('home-page.png');
-    browser.saveScreenshot('/out/home-page.png', true);
+    browser.saveDocumentScreenshot('/out/home-page.png');
+    //browser.saveScreenshot('/out/home-page.png', true);
 
     console.log('After screenshot');
 
